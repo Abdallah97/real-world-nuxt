1b06ed137c1794ddefcf92028eb6b5bf479d0fd0
Abdallah97 <abdallah.salah9700@gmail.com>
2022-12-17 19:04:24 +0200

    service

--- services/EventService.js
+++ services/EventService.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `https://my-json-server.typicode.com/AliciadW/scaling-vue-with-nuxt`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
